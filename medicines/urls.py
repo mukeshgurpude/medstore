9914d6898fbab85d8737747ac7acaebd97b8d8a4
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2020-08-22 14:14:29 +0530

    finished all the user views for medicine. requires styling

--- medicines/urls.py
+++ medicines/urls.py
@@ -5,4 +5,8 @@ app_name = "medicines"
 
 urlpatterns = [
     path("", views.MedListView.as_view(), name="all"),
+    path("medicine/<int:pk>/", views.MedDetailView.as_view(), name="detail"),
+    path("add/", views.MedCreateView.as_view(success_url=reverse_lazy("medicines:all")), name="create"),
+    path("update/<int:pk>/", views.MedUpdateView.as_view(success_url=reverse_lazy("medicines:all")), name="update"),
+    path("delete/<int:pk>/", views.MedDeleteView.as_view(success_url=reverse_lazy("medicines:all")), name="delete"),
 ]
