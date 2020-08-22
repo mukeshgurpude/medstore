2676bbab475b386586f98440d79526ebd239060b
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2020-08-22 14:48:03 +0530

    Added homepage, styling required.

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from.views import HomeView
+
+app_name = 'home'
+
+urlpatterns = [
+    path("", HomeView.as_view())
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.conf import settings
 # Create your views here.
+
+
+class HomeView(View):
+    def get(self, request, ):
+        ctx = {
+            'installed': settings.INSTALLED_APPS,
+        }
+        return render(request, "home/main.html", ctx)
