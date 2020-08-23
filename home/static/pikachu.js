e41ac6230313557a8e45d6131890cf6120edb26b
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2020-08-23 12:11:29 +0530

    created custom JS

---
+++ home/static/pikachu.js
@@ -0,0 +1,8 @@
+ids = ["id_name", "id_category", "id_description", "id_quantity", "id_price"]
+for(id in ids){
+    $("#"+ids[id]).addClass("form-control");
+}
+
+$("#id_thumbnail").addClass("form-control-file")
+$("input[value=Submit").addClass("btn btn-default")
+$("input[value=Cancel").addClass("btn btn-danger")
