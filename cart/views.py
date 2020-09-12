f5fb52d4d7bdf0d2ae9e3f9b3a7ddace2e42279f
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2020-09-12 12:54:37 +0530

    async loading bootstrap theme

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from .models import Order, CartItem
 from medicines.models import Medicine
 from django.contrib import messages
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -51,14 +52,9 @@ def remove_from_cart(request, slug):
         order = orders[0]
         if order.items.filter(item__slug=item.slug).exists():
             order_item = CartItem.objects.filter(user=request.user, item=item)[0]
-            if order_item.quantity > 1:
-                order_item.quantity -= 1
-                order_item.save()
-                messages.info(request, "Item quantity updated")
-            else:
-                order.items.remove(order_item)
-                order_item.delete()
-                messages.success(request, "Item deleted from the cart")
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.success(request, "Item deleted from the cart")
         else:
             messages.error(request, "Item was not in your cart")
     else:
@@ -120,3 +116,52 @@ def order_view(request):
     orders = Order.objects.filter(user=request.user, ordered=True)
     ctx = {"orders": orders}
     return render(request, "order/view.html", ctx)
+
+
+def increase_cart_test(request, slug):
+    a = request.META.get("HTTP_REFERER")
+    # if not a:
+    #     messages.error(request, "You cannot edit cart items this way")
+    #     return redirect(reverse_lazy("medicines:all"))
+    q = 0
+    item = get_object_or_404(Medicine, slug=slug)
+    order_item = CartItem.objects.get(user=request.user, item=item)
+    orders = Order.objects.filter(user=request.user, ordered=False)
+    if orders.exists():
+        order = orders[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item quantity updated.")
+            q = order_item.quantity
+        else:
+            messages.error(request, "Item was not in the cart")
+    else:
+        messages.error(request, "Your cart is empty")
+    return JsonResponse({'quantity': q})
+
+
+def decrease_cart_test(request, slug):
+    a = request.META.get("HTTP_REFERER")
+    if not a:
+        messages.error(request, "You cannot edit cart items this way")
+        return redirect(reverse_lazy("medicines:all"))
+    item = get_object_or_404(Medicine, slug=slug)
+    orders = Order.objects.filter(user=request.user, ordered=False)
+    if orders.exists():
+        order = orders[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = CartItem.objects.filter(user=request.user, item=item)[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+                messages.info(request, "Item quantity updated")
+            else:
+                order.items.remove(order_item)
+                order_item.delete()
+                messages.success(request, "Item deleted from the cart")
+        else:
+            messages.error(request, "Item was not in your cart")
+    else:
+        messages.error(request, "your cart is empty")
+    return redirect(request.META.get("HTTP_REFERER"))
