0ea08dfd2f88b9a9ae1e0a04eb9d081a34b8a994
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2020-09-12 12:54:37 +0530

    moving towards SEO

--- cart/urls.py
+++ cart/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("cart/", cartview, name="home"),
     path("add/<slug>", add_to_cart, name="add"),
     path("remove/<slug>", remove_from_cart, name="remove"),
-    path("increase/<slug>", views.increase_cart, name="increase"),
+    path("increase/<slug>", views.increase_cart_test, name="increase"),
     path("decrease/<slug>", views.decrease_cart, name="decrease"),
     path("orders/", views.order_view, name="my_orders")
 ]
