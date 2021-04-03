a9f5f01374477e587b4c8367044365c5921e0fe4
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-04-03 09:46:10 +0530

    restructured api views ^_^

---
+++ api/views/__init__.py
@@ -0,0 +1,2 @@
+# A little bit modification, used the __init__.py as to make different routes in different files
+from .medicine import *
--- api/views/medicine.py
+++ api/views/medicine.py
@@ -1,7 +1,5 @@
-from django.views.generic import View
-from django.http import HttpResponse, JsonResponse
-from medicines.models import Medicine, MedCat
-from . import serializers
+from django.http import JsonResponse
+from medicines.models import Medicine
 from django.core.serializers.json import Serializer
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -11,30 +9,11 @@ from medicines.views import MedCreateView
 # Create your views here.
 
 # API Version 1
-# Without rest framework, #SASTA
-
-
-class MedicineView(View):
-
-    def get_queryset(self, *args, **kwargs):
-        return Medicine.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        qs = self.get_queryset()
-        serialized = Serializer().serialize(qs, fields=('name', 'price'))
-        return JsonResponse(serialized, safe=False)
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
-    def post(self, request):
-        s = serializers.MedicineSerializer(data=request.POST)
-        print(s.is_valid())
-        return HttpResponse('yes')
+# Without rest framework, {#SASTA}
 
 
 class CreateView(MedCreateView):
+    # Inherited from MedCreateView, as to incorporate the Login required and Permission required Mixin
 
     def get(self, request, pk=None):
         form = CreateForm()
@@ -43,9 +22,9 @@ class CreateView(MedCreateView):
         return JsonResponse({'fields': form_details}, status=200)
 
     def post(self, request, pk=None):
+        # Same process as the MedCreateView, but with customization for the JSON response
         form = CreateForm(request.POST, request.FILES or None)
         if not form.is_valid():
-            print(form)
             fields = form.base_fields
             form_details = [field for field in fields.keys()]
             return JsonResponse({'fields': form_details}, status=200)
@@ -55,9 +34,33 @@ class CreateView(MedCreateView):
         return JsonResponse({'msg': 'created', 'newID': med.id}, status=201)
 
 
-# INFO: This function will return current user details in json format
+class MedicineView(CreateView):
+    # As this is inherited from { CreateView }, POST requests on this
+    # view will also be able to add a new medicine
+    def get_queryset(self, *args, **kwargs):
+        # It's better to use this method separately, as we might want to do another queries,
+        # or may need to order the data, or may need to sort the data, all of this will be
+        # handled in this method
+
+        # TODO: Handle the filters
+        return Medicine.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        qs = self.get_queryset()
+        serialized = Serializer().serialize(qs, fields=('name', 'price'))
+        return JsonResponse(serialized, safe=False)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        # Just to turn off the csrf verification for this view only...
+        # In future, another ( process | algorithm | method ) must be incorporated without blocking
+        # the CSRF
+        return super().dispatch(request, *args, **kwargs)
+
+
 def get_current_user(request):
     """
+    This function will return current user details in json format
     TODO: Add DocString
     """
     if request.method != 'GET':
@@ -73,3 +76,5 @@ def get_current_user(request):
                              'email': request.user.email,
                              'loggedIn': not user.is_anonymous
                              }, status=200)
+
+
