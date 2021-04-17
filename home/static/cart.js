cb616411fa5c65c6283acdcbae7c1d2160b6a3aa
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-04-18 00:22:07 +0530

    Added a footer row with details

--- home/static/cart.js
+++ home/static/cart.js
@@ -1,5 +1,4 @@
 const loader = document.getElementById("quickloader");
-const page = document.getElementsByTagName("div")[1];
 
 const incdecbuttons = document.querySelectorAll('td>a[role=button]')
 function changeValue(e){
@@ -13,16 +12,34 @@ function changeValue(e){
             if(response["quantity"]===0){document.querySelector(`#row${itemId}`).remove()}
             else{
                 document.querySelector(`span#sc${itemId}`).textContent = response["quantity"];
-                const total = Math.floor(response['quantity']*price*100)/100;
+                const total = Math.round(response['quantity']*price*100)/100;
                 document.querySelector(`#row${itemId}>.total`).textContent = String(total);
             }
             loader.style.display = "none";
+            aggregate()
         })
         .catch(_=>{
             loader.style.display = 'none';
+            aggregate();
         });
 }
 
-incdecbuttons.forEach(button=>{
-    button.addEventListener('click', changeValue);
+const aggregate = _ => {
+    const quantities = document.querySelectorAll('.quantity');
+    const costs = document.querySelectorAll('.total');
+    const agg = document.querySelector('#aggregate');
+
+    agg.querySelector('td:nth-child(2)').textContent = String(Array.from(quantities)
+        .reduce((sum, i)=>sum+parseFloat(i.textContent), 0))
+
+    const total = String(Array.from(costs)
+        .reduce((sum, i)=>sum+parseFloat(i.textContent), 0))
+    agg.querySelector('td:nth-child(4)').textContent = String(Math.round(total*100)/100)
+}
+
+window.addEventListener('load', function(){
+    incdecbuttons.forEach(button=>{
+        button.addEventListener('click', changeValue);
+    })
+    aggregate();
 })
