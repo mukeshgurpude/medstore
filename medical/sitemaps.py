838acfddf149c0dad844ee5289f83f11abe5f886
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-04-27 08:29:10 +0000

    added little documentatiion

--- medical/sitemaps.py
+++ medical/sitemaps.py
@@ -1,11 +1,16 @@
-# Top level sitemap which generates sitemap for all static urls
-# (except which are generated elsewhere)
+""" 
+Top level sitemap which generates sitemap for all static urls
+(except which are generated elsewhere)
+"""
 
 from django.contrib import sitemaps
 from django.urls import reverse
 
 
 class StaticSitemap(sitemaps.Sitemap):
+    """
+    Url paths to static urls
+    """
     changeFreq = 'monthly'
     priority = .8
 
@@ -14,6 +19,7 @@ class StaticSitemap(sitemaps.Sitemap):
             "cart": ['home', 'my_orders'],
             "checkout": ['order'],
             "home": ['home'],
+            "medicines": ["all"]
         }
         return [app + ":" + view for app, views in pages.items() for view in views]
 
