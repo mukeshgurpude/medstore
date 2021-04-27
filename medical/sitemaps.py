001e7d55ce5e6ea2939631e5ff6cba8b985bfc3f
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-04-27 13:05:57 +0530

    added other sitemaps

---
+++ medical/sitemaps.py
@@ -0,0 +1,21 @@
+# Top level sitemap which generates sitemap for all static urls
+# (except which are generated elsewhere)
+
+from django.contrib import sitemaps
+from django.urls import reverse
+
+
+class StaticSitemap(sitemaps.Sitemap):
+    changeFreq = 'monthly'
+    priority = .8
+
+    def items(self):
+        pages = {
+            "cart": ['home', 'my_orders'],
+            "checkout": ['order'],
+            "home": ['home'],
+        }
+        return [app + ":" + view for app, views in pages.items() for view in views]
+
+    def location(self, page):
+        return reverse(page)
--- medical/urls.py
+++ medical/urls.py
@@ -4,16 +4,18 @@ from django.contrib.sitemaps.views import sitemap
 from medicines.sitemaps import MedSiteMap
 from accounts.sitemaps import AccountSiteMap
 from django.views.generic import TemplateView
+from .sitemaps import StaticSitemap
 
 
 sitemaps = {
     'accounts': AccountSiteMap,
     'medicines': MedSiteMap,
+    'other': StaticSitemap
 }
 
 urlpatterns = [
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
-    path("sitemap.xml/", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
     path("", include("home.urls")),
     path("", include("cart.urls")),
     path('admin/', admin.site.urls),
