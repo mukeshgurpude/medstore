e28b4e1fe24c7feacd452d649c0187fb3e8fcd6d
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-05-28 11:58:07 +0530

    configured service worker path

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from.views import HomeView
 
 app_name = 'home'
 
 urlpatterns = [
-    path("", HomeView.as_view(), name='home')
+    path("", HomeView.as_view(), name='home'),
+    path(r'service-worker.js', (TemplateView.as_view(
+        template_name="sw.js",
+        content_type='application/javascript',
+    )), name='service-worker.js')
 ]
